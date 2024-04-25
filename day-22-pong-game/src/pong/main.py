64b11e6033aa0f19bc99ed224ff5a75be11240f1
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2024-04-25 21:49:23 +0800

    day 22

---
+++ day-22-pong-game/src/pong/main.py
@@ -0,0 +1,59 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+from time import sleep
+
+L_PADDLE_START_POS = (-350, 0)
+R_PADDLE_START_POS = (350, 0)
+
+L_PADDLE_MOVE_UP = "w"
+L_PADDLE_MOVE_DOWN = "s"
+R_PADDLE_MOVE_UP = "Up"
+R_PADDLE_MOVE_DOWN = "Down"
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong game")
+screen.tracer(0)
+
+l_paddle = Paddle(L_PADDLE_START_POS)
+r_paddle = Paddle(R_PADDLE_START_POS)
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(fun=l_paddle.move_up, key=L_PADDLE_MOVE_UP)
+screen.onkeypress(fun=l_paddle.move_down, key=L_PADDLE_MOVE_DOWN)
+screen.onkeypress(fun=r_paddle.move_up, key=R_PADDLE_MOVE_UP)
+screen.onkeypress(fun=r_paddle.move_down, key=R_PADDLE_MOVE_DOWN)
+
+is_game_on = True
+while is_game_on:
+    sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.xcor() == 330 and ball.distance(r_paddle.position()) < 60 or ball.xcor() == -330 \
+            and ball.distance(l_paddle.position()) < 60:
+        ball.bounce_x()
+
+    # Detect left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.increase_score_right_paddle()
+
+    # Detect right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.increase_score_left_paddle()
+
+    if scoreboard.has_a_winner():
+        is_game_on = False
+
+scoreboard.game_over()
+screen.exitonclick()
