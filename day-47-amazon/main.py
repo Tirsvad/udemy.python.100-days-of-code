161bca4e936021154f966b181388fe8a3916fd7e
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2024-06-03 10:42:29 +0800

    Getting the right price

--- day-47-amazon/main.py
+++ day-47-amazon/main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+from lxml import etree
 from constants import *
 
 response = requests.get(url=PRODUCT_END_POINT, params=PARAMS, headers=HEADERS)
@@ -8,17 +9,16 @@ response.raise_for_status()
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-# price = soup.find_all(name="span", class_="a-offscreen")
-price = soup.find(name='span', attrs={"class": "priceToPay"})
-# print(len(price))
+price = soup.find_all(name="span", attrs={"class": "a-offscreen"})[1].getText().split("$")[1]
+
 print(price)
 
-if float(price) < 89:
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=EMAIL_USER, password=PASSWORD)
-        connection.sendmail(
-            from_addr=EMAIL_FROM,
-            to_addrs=EMAIL_SEND_TO,
-            msg=f"Subject:Product on amazon now under the price setting\n\n{PRODUCT_END_POINT} at price {price}"
-        )
+# if float(price[1]) < 89:
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(user=EMAIL_USER, password=PASSWORD)
+#         connection.sendmail(
+#             from_addr=EMAIL_FROM,
+#             to_addrs=EMAIL_SEND_TO,
+#             msg=f"Subject:Product on amazon now under the price setting\n\n{PRODUCT_END_POINT} at price {price}"
+#         )
